9cbda6dfb2b1278b0f60ac614e1cc87a2e8781d8
GD HIRUNA <61905872+hirunaofficial@users.noreply.github.com>
2021-06-19 08:30:39 +0530

    Covid-19-Statistics-Telegram Bot
    Covid-19-Statistics-Telegram Bot 💉 is a Telegram Bot you can Track all about Covid 19 Live Statistics in Sri Lanka 🇱🇰

---
+++ CovidBot.php
@@ -0,0 +1,67 @@
+<?php
+$token = 'BOT_TOKEN';
+$img = 'BOT_IMG';
+
+$input = file_get_contents('php://input');
+$update = json_decode($input);
+$send = $update->message->text;
+$chat_id = $update->message->chat->id;
+$fname = $update->message->chat->first_name;
+$lname = $update->message->chat->last_name;
+
+if (strpos($send, "/start") === 0) {
+
+$welcome = urlencode("<b>\xF0\x9F\x92\x89 Sri Lanka Covid 19 Stats Bot \xF0\x9F\x92\x89</b>\n\n\xF0\x9F\x91\x8B	Hey <b>$fname $lname</b> \xf0\x9f\x87\xb1\xf0\x9f\x87\xb0 I'm <b>Sri Lanka Covid 19 Stats Bot</b> \xF0\x9F\x92\xAA Type /covid or /corona to get all about <b>Covid 19</b> Informations \xF0\x9F\x92\xA1 Powerd by <b>Sri Lanka Health Promotion Bureau</b> \xF0\x9F\x92\x8A and Send /help to Get more Information About <b>Sri Lanka Covid 19 Stats Bot</b> \xE2\x9A\xA1");
+
+file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$welcome");
+}
+
+if (strpos($send, "/help") === 0) {
+
+$help = urlencode("<b>\xF0\x9F\x92\x89 Sri Lanka Covid 19 Stats Bot \xF0\x9F\x92\x89</b>\n\n\xE3\x80\xBD <b>About Bot</b> \xE3\x80\xBD\n\n<b>\xE2\x96\xB6	Name</b> - Sri Lanka Covid 19 Stats Bot\n<b>\xE2\x96\xB6	Username</b> - @srilankacovid19statsbot\n<b>\xE2\x96\xB6	Created By</b> - @hirunaofficial\n\n<b>\xF0\x9F\x94\xA7 Bot Commands \xF0\x9F\x94\xA7</b>\n\n\xE2\x96\xB6	/start - What is Sri Lanka Covid 19 Stats Bot\n\xE2\x96\xB6	/covid /corona - Get Local and Globle Covid 19 Informations\n\xE2\x96\xB6	/help - More information about Sri Lanka Covid 19 Stats Bot\n\n<b>\xE2\x9E\x95 Add Bot to Your Group \xE2\x9E\x95</b>\n\n\xE2\x96\xB6	You need to add Sri Lanka Covid 19 Bot to Your Group First go to your Group and Go to Add Members Section Type @srilankacovid19statsbot and Select Bot Now you are Successfully added Sri lanka Covid 19 Stats Bot to your group now you can get all about Covid 19 Information using type /covid or /corona");
+
+file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$help");
+}
+
+if (strpos($send, "/covid") === 0) {
+
+$url = file_get_contents('https://www.hpb.health.gov.lk/api/get-current-statistical');
+$coviddecode = json_decode($url);
+$update = $coviddecode->data->update_date_time;
+$lnew = $coviddecode->data->local_new_cases;
+$ltotal = $coviddecode->data->local_total_cases;
+$lactive = $coviddecode->data->local_active_cases;
+$ldeaths = $coviddecode->data->local_deaths;
+$lrecovered = $coviddecode->data->local_recovered;
+$lhospitals = $coviddecode->data->local_total_number_of_individuals_in_hospitals;
+$gnew = $coviddecode->data->global_new_cases;
+$gtotal = $coviddecode->data->global_total_cases;
+$gdeaths = $coviddecode->data->global_deaths;
+$grecovered = $coviddecode->data->global_recovered;
+
+$covid = urlencode("<b>\xF0\x9F\x92\x89 Sri Lanka Covid 19 Stats Bot \xF0\x9F\x92\x89\n(Date/Time - $update)</b>\n\n<b>\xf0\x9f\x87\xb1\xf0\x9f\x87\xb0 Local Covid 19 Stats Bot \xf0\x9f\x87\xb1\xf0\x9f\x87\xb0</b>\n\n\xF0\x9F\x93\x8A Total Confirmed Cases: $ltotal\n\xF0\x9F\x94\x96 Active Cases: $lactive\n\xF0\x9F\x98\xB7 Daily New Cases: $lnew\n\xF0\x9F\x8F\xA5 Individuals currently under investigations in hospitals: $lhospitals\n\xF0\x9F\x93\x9F	 Recovered & Discharged: $lrecovered\n\xF0\x9F\x92\xA5 Deaths: $ldeaths\n\n<b>\xF0\x9F\x8C\x8E Globle Covid 19 Stats Bot \xF0\x9F\x8C\x8E</b>\n\n\xF0\x9F\x93\x8A Total Confirmed Cases: $gtotal\n\xF0\x9F\x98\xB7 Daily New Cases: $gnew\n\xF0\x9F\x93\x9F Recovered:$grecovered\n\xF0\x9F\x92\xA5 Deaths: $gdeaths");
+
+file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$covid");
+}
+
+if (strpos($send, "/corona") === 0) {
+
+$url = file_get_contents('https://www.hpb.health.gov.lk/api/get-current-statistical');
+$coviddecode = json_decode($url);
+$update = $coviddecode->data->update_date_time;
+$lnew = $coviddecode->data->local_new_cases;
+$ltotal = $coviddecode->data->local_total_cases;
+$lactive = $coviddecode->data->local_active_cases;
+$ldeaths = $coviddecode->data->local_deaths;
+$lrecovered = $coviddecode->data->local_recovered;
+$lhospitals = $coviddecode->data->local_total_number_of_individuals_in_hospitals;
+$gnew = $coviddecode->data->global_new_cases;
+$gtotal = $coviddecode->data->global_total_cases;
+$gdeaths = $coviddecode->data->global_deaths;
+$grecovered = $coviddecode->data->global_recovered;
+
+$covid = urlencode("<b>\xF0\x9F\x92\x89 Sri Lanka Covid 19 Stats Bot \xF0\x9F\x92\x89\n(Date/Time - $update)</b>\n\n<b>\xf0\x9f\x87\xb1\xf0\x9f\x87\xb0 Local Covid 19 Stats Bot \xf0\x9f\x87\xb1\xf0\x9f\x87\xb0</b>\n\n\xF0\x9F\x93\x8A Total Confirmed Cases: $ltotal\n\xF0\x9F\x94\x96 Active Cases: $lactive\n\xF0\x9F\x98\xB7 Daily New Cases: $lnew\n\xF0\x9F\x8F\xA5 Individuals currently under investigations in hospitals: $lhospitals\n\xF0\x9F\x93\x9F	 Recovered & Discharged: $lrecovered\n\xF0\x9F\x92\xA5 Deaths: $ldeaths\n\n<b>\xF0\x9F\x8C\x8E Globle Covid 19 Stats Bot \xF0\x9F\x8C\x8E</b>\n\n\xF0\x9F\x93\x8A Total Confirmed Cases: $gtotal\n\xF0\x9F\x98\xB7 Daily New Cases: $gnew\n\xF0\x9F\x93\x9F Recovered:$grecovered\n\xF0\x9F\x92\xA5 Deaths: $gdeaths");
+
+file_get_contents("https://api.telegram.org/bot$token/sendphoto?chat_id=$chat_id&photo=$img&parse_mode=HTML&caption=$covid");
+}
+?>
--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-<h1 align="center">Covid 19 Statistics Web</h3>
+<h1 align="center">Covid-19-Statistics-Telegram Bot 💉</h3>
 <p align="center">
-  <a href="https://github.com/hirunaofficial/Covid-19-Statistics-Web">
-    <img src="https://socialify.git.ci/hirunaofficial/Covid-19-Statistics-Web/image?description=1&forks=1&issues=1&language=1&owner=1&pattern=Floating%20Cogs&pulls=1&stargazers=1&theme=Dark" alt="Cover Image" width="650">
+  <a href="https://github.com/hirunaofficial/Covid-19-Statistics-Telegram-Bot">
+    <img src="https://socialify.git.ci/hirunaofficial/Covid-19-Statistics-Telegram-Bot/image?description=1&forks=1&issues=1&language=1&owner=1&pattern=Floating%20Cogs&pulls=1&stargazers=1&theme=Dark" alt="Cover Image" width="650">
   </a>
 </p>
 
+## How to use the bot
+ 
+`/start` : To check if the bot is alive or not.
+
+`/help` : To View Bot Commands and Bot Informations.
+
+`/covid` : To get All About Covid 19 Informations.
+`/corona` : To get All About Covid 19 Informations.
 
 # What is VPS Statistics Web ?
-Covid 19 Statistics Web is a Simple Website to Track Covid 19 Live Statistics in Sri Lanka.
+Covid-19-Statistics-Telegram Bot 💉 is a Telegram Bot you can Track all about Covid 19 Live Statistics in Sri Lanka 🇱🇰
 
 # What does it show ?
-* Local Total Confirmed Cases
-* Local Active Cases
-* Local Daily New Cases
-* Local Individuals currently under investigations in hospitals
-* Local Recovered & Discharged
-* Globle Total Confirmed Cases
-* Globle Daily New Cases
-* Globle Recovered
-* Globle Deaths
+* Local Covid 19 Informations
+* Globle Covid 19 Informations
